71e5fdf2ad28f34a363029df809147e3a20c38be
Cris Ovalle <cris.ovalle@tagmagnet.org>
2025-09-08 16:38:32 -0700

    wrote small programs in the Card class

--- src/flippy/Card.java
+++ src/flippy/Card.java
@@ -16,35 +16,35 @@ public class Card {
 	private boolean faceUp = false; // whether the card is face up
 
 	/**
-	 * Create a new card with number and suit.  If the a valid
+	 * Create a new card with number and suit. If the a valid
 	 * suit/number is not input, the card defaults to 1 of hearts.
 	 * 
 	 * @param number the card number
-	 * @param suit the card suit
+	 * @param suit   the card suit
 	 */
-	public Card(int number, String suit){
+	public Card(int number, String suit) {
 		this.number = number;
 		this.suit = suit.toLowerCase();
 
-		if( !isValidSuit(suit) ){
+		if (!isValidSuit(suit)) {
 			System.out.println(suit + "Is not a valid suit!");
 			System.out.println("Must be one of: clubs, diamonds, hearts or spades");
 			System.out.println("Defaulting to hearts");
 			this.suit = "hearts";
 		}
 
-		if( !isValidNumber(number) ){
+		if (!isValidNumber(number)) {
 			System.out.println(number + "Is not a valid number!");
 			System.out.println("Must be between " + MIN_NUMBER + " and " + MAX_NUMBER);
 			System.out.println("Defaulting to 1");
 			this.number = 1;
-		}		
+		}
 	}
 
-	/** 
+	/**
 	 * @return the card's suite
 	 */
-	public String getSuit(){
+	public String getSuit() {
 		return suit;
 	}
 
@@ -54,46 +54,46 @@ public class Card {
 	 * 
 	 * @return the cards number
 	 */
-	public String getNumber(){
-		if( number == 1 ){
+	public String getNumber() {
+		if (number == 1) {
 			return "Ace";
-		}else if( number >= 2 && number <= 10 ){
+		} else if (number >= 2 && number <= 10) {
 			return Integer.toString(number);
-		}else if( number == 11 ){
+		} else if (number == 11) {
 			return "Jack";
-		}else if( number == 12 ){
+		} else if (number == 12) {
 			return "Queen";
-		}else{
+		} else {
 			return "King";
 		}
 	}
-	
+
 	/**
 	 * @return whether or no the card is face up
 	 */
-	public boolean isFaceUp(){
+	public boolean isFaceUp() {
 		return faceUp;
 	}
-	
+
 	/**
-	 * Flip the card over.  If the card is face down it becomes
+	 * Flip the card over. If the card is face down it becomes
 	 * face up and vice versa.
 	 */
-	public void flip(){
+	public void flip() {
 		faceUp = !faceUp;
 	}
-	
+
 	/**
 	 * The cards number followed by the suit
 	 */
-	public String toString(){
+	public String toString() {
 		return getNumber() + " of " + getSuit();
 	}
-	
+
 	/**
 	 * Make it an Ace!
 	 */
-	public void cheat(){
+	public void cheat() {
 		number = 1; // ACE!
 	}
 
@@ -103,7 +103,7 @@ public class Card {
 	 * @param num potential card number
 	 * @return whether the card number is valid
 	 */
-	private boolean isValidNumber(int num){
+	private boolean isValidNumber(int num) {
 		return num >= MIN_NUMBER && num <= MAX_NUMBER;
 	}
 
@@ -113,13 +113,13 @@ public class Card {
 	 * @param s potential suit
 	 * @return whether the suit is valid
 	 */
-	private boolean isValidSuit(String s){
+	private boolean isValidSuit(String s) {
 		return s.equals("spades") ||
 				s.equals("hearts") ||
 				s.equals("clubs") ||
 				s.equals("diamonds");
 	}
-	
+
 	/**
 	 * The value of this card for the flippy card game.
 	 * Numbers have their number as the value, Ace is 11,
@@ -127,18 +127,27 @@ public class Card {
 	 * 
 	 * @return the flippy card value
 	 */
-	public int getFlippyCardValue(){
-		//TODO: Fill in good stuff here!
-		return 0;
+	public int getFlippyCardValue() {
+		// TODO: Fill in good stuff here!
+		if (number == 1) {
+			return 11;
+		}
+		if (number == 11 || number == 12 || number == 13) {
+			return 10;
+		}
+		return number;
 	}
-	
+
 	/**
 	 * Whether the suit is red or not.
 	 * 
 	 * @return whether or not the card is red
 	 */
-	public boolean isRedCard(){
-		//TODO: Fill in good stuff here!
+	public boolean isRedCard() {
+		// TODO: Fill in good stuff here!
+		if (suit == "hearts" || suit == "diamonds") {
+			return true;
+		}
 		return false;
 	}
 }
